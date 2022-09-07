66126a79d6afe42eb4238bdd87b3b6519c6e8b5a
kpenfound <kylepenfound@pm.me>
2022-09-06 20:05:45 -0400

    add generated cloak files

--- go.mod
+++ go.mod
@@ -3,11 +3,9 @@ module github.com/kpenfound/go-desk
 go 1.16
 
 require (
-	github.com/99designs/gqlgen v0.17.16 // indirect
-	github.com/Khan/genqlient v0.5.0 // indirect
+	github.com/Khan/genqlient v0.5.0
 	github.com/aws/aws-sdk-go v1.38.15
-	github.com/dagger/cloak v0.0.0-20220906161451-1c7c9f8f035e // indirect
-	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dagger/cloak v0.0.0-20220906161451-1c7c9f8f035e
 	github.com/mitchellh/cli v1.1.2
 )
 
--- magefiles/main.go
+++ magefiles/main.go
@@ -3,7 +3,15 @@
 package main
 
 import (
+	"os"
+	"fmt"
+	"context"
 
+	"github.com/dagger/cloak/engine"
+	"github.com/dagger/cloak/sdk/go/dagger"
+
+	"github.com/kpenfound/go-desk/magefiles/gen/core"
+	"github.com/kpenfound/go-desk/magefiles/gen/netlify"
 )
 
 // Build and deploy the api to AWS Lambda
@@ -18,14 +26,24 @@ func Infra(ctx context.Context) {
 
 // Build and deploy the website to Netlify
 func Website(ctx context.Context) {
-	netlifyToken, ok := os.LookupEnv("NETLIFY_AUTH_TOKEN")
+	if err := engine.Start(ctx, &engine.Config{}, func(ctx engine.Context) error {
+		// User can configure netlify site name with $NETLIFY_SITE_NAME
+		siteName, ok := os.LookupEnv("NETLIFY_SITE_NAME")
+		if !ok {
+			user, _ := os.LookupEnv("USER")
+			siteName = fmt.Sprintf("%s-dagger-todoapp", user)
+		}
+		fmt.Printf("Using Netlify site name %q\n", siteName)
+
+		// User must configure netlify API token with $NETLIFY_AUTH_TOKEN
+		tokenCleartext, ok := os.LookupEnv("NETLIFY_AUTH_TOKEN")
 		if !ok {
 			return fmt.Errorf("NETLIFY_AUTH_TOKEN not set")
 		}
 
 		// Load API token into a secret
 		var token dagger.SecretID
-		if resp, err := core.AddSecret(ctx, netlifyToken); err != nil {
+		if resp, err := core.AddSecret(ctx, tokenCleartext); err != nil {
 			return err
 		} else {
 			token = resp.Core.AddSecret
@@ -39,14 +57,14 @@ func Website(ctx context.Context) {
 			source = resp.Host.Workdir.Read.ID
 		}
 
-		// Deploy using the todoapp deploy extension
-		resp, err := todoapp.Deploy(ctx, source, siteName, token)
+		// Deploy using the netlify deploy extension
+		resp, err := netlify.Deploy(ctx, source, siteName, token)
 		if err != nil {
 			return err
 		}
 
 		// Print deployment info to the user
-		fmt.Println("URL:", resp.Todoapp.Deploy)
+		fmt.Println("URL:", resp.Netlify.Deploy)
 
 		return nil
 	}); err != nil {
